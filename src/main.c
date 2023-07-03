663dfcb9561ea97237ac3d4bc52beec796f14b82
Sammy Gluck <infogls@yahoo.com>
2023-07-03 15:21:10 +0200

    latest commit

--- src/main.c
+++ src/main.c
@@ -1,1 +1,88 @@
 #include "so_long.h"
+
+typedef struct s_map
+{
+    char *map;
+    int x;
+    int y;
+} t_map;
+
+//is this the optimal solution?
+void strip_newline(char *line)
+{
+    int i;
+
+    i = 0;
+    while (line[i])
+    {
+        if (line[i] == '\n')
+            line[i] = '\0';
+        i++;
+    }
+}
+
+int add_line_to_map(char *line, t_map *map)
+{
+    //we also have to strip the newline characters at the end
+    strip_newline(line);
+
+    //first line 
+    //we don't need to join to any previous lines
+    //what is the x value
+    if (map->y == 0)
+    {
+        map->map = malloc(ft_strlen(line));
+        //make sure we deal correctly with the error
+        if (!(map->map))
+            return (0);
+        //check for the length of the line
+        map->x = ft_strlen(line);
+        //validate the length (make sure it's the minimum required length) at least 4 if not more
+
+        //copy the string to the map->map stuff 
+        map->map = ft_strdup(line);
+         //should we free line now or in the parent of this function (current implemenation: parent at the end)
+    }
+
+    //other lines
+    //check if each x value is the same as first
+    // join to the already existing string
+    
+}
+
+
+char    *init_map()
+{
+    t_map   *map;
+    char *line;
+    int fd;
+
+    fd = open("map.ber", O_RDONLY);
+    //error check
+    map = malloc(sizeof t_map);
+    if (!map)
+        ft_printf("error to be handled still");
+    //might need to find a better place for this potentially:
+    map->x = 0;
+    map->y = 0;
+    
+    while (1)
+    {
+       
+        line = get_next_line(fd);
+        if (!line)
+            break;
+        add_line_to_map(line, map);
+        free(line);
+        //make sure to add to the y value each time
+        map->y += 1;
+    }
+    //call gnl until end
+    //for every call we'll join unto the previous string
+    //we also have to differentiate between the first, last and rest of the strings
+}
+
+int main(int argc, char **argv)
+{
+    init_map();
+}
--- src/so_long.h
+++ src/so_long.h
@@ -2,6 +2,10 @@
 #define SO_LONG_H
 
 #include "../libft/libft.h"
+#include "../ft_printf/ft_printf.h"
+#include "../gnl/get_next_line.h"
+#include "../mlx_linux/mlx.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
