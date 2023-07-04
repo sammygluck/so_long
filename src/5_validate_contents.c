b256148437e845dcf89b6ed792b1ae6b8a9213e5
Sammy Gluck <infogls@yahoo.com>
2023-07-04 16:07:12 +0200

    validate contents

--- src/5_validate_contents.c
+++ src/5_validate_contents.c
@@ -1,2 +1,47 @@
 #include "so_long.h"
 
+int get_char_count(char *string, char character)
+{
+    int i;
+    int count;
+
+    i = 0;
+    count = 0;
+    while (string[i])
+    {
+        if (string[i] == character)
+            count++;
+        i++;
+    }
+    return (count);
+}
+
+int all_valid_char(char *string)
+{
+    int i;
+
+    i = 0;
+    while (string[i])
+    {
+        if(!ft_strchr("CEP10", string[i]))
+            return (0);
+        i++;
+    }
+    return (1);
+}
+
+int validate_contents(t_map *map)
+{
+    if (!all_valid_char(map->map))
+        return (0);
+    if (get_char_count(map->map, 'C') < 1)
+        return (0);
+    if (get_char_count(map->map), 'E' != 1)
+        return (0);
+    if (get_char_count(map->map, 'P') != 1)
+        return (0);
+    
+    //also set the character count in the variable in the future struct to be made.
+    return (1);
+
+}
