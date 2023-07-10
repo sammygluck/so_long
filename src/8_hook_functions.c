d3801934892e40ecb146875a861286de7ce220f0
Sammy Gluck <infogls@yahoo.com>
2023-07-10 16:08:24 +0200

    basic keycode mappings

--- src/8_hook_functions.c
+++ src/8_hook_functions.c
@@ -2,8 +2,14 @@
 
 int hook_actions(int keycode, t_game *game)
 {
-    printf("%i\n", keycode);
+    if (keycode == 97 || keycode == 65361)
+        ft_printf("we're going left\n");
+    else if (keycode == 119 || keycode == 65361)
+        ft_printf("we're going up\n");
+    else if (keycode == 100 || keycode == 65363)
+        ft_printf("we're going right\n");
+    else if (keycode == 115 || keycode == 65364)
+        ft_printf("we're going down\n");
     printf("%i\n", game->map->x);
-    printf("hello it works");
     return (0);
 }
