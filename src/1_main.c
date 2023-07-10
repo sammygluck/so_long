de6c117175a1e962d707ff0d4a9a223aaef49f2f
Sammy Gluck <infogls@yahoo.com>
2023-07-10 15:12:21 +0200

    update

--- src/1_main.c
+++ src/1_main.c
@@ -3,15 +3,15 @@
 int main(int argc, char **argv)
 {
     t_map   *map;
-    t_master data;
+    t_game game;
     
     check_input(argc, argv);
     map = malloc(sizeof (t_map));
     if (!map)
         ft_printf("error to be handled still");
     init_map(map);
-    data.map = map;
-    map_to_window(data.map);
-    ft_printf("\ndatamap: %s", data.map->map);
+    game.map = map;
+    map_to_window(&game);
+    ft_printf("\ndatamap: %s", game.map->map);
     //map_to_window();
 }
