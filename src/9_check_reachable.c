a2ab5f070c238700b92cbb62ee921febaadd1062
Sammy Gluck <infogls@yahoo.com>
2023-07-11 16:47:43 +0200

    error testing

--- src/9_check_reachable.c
+++ src/9_check_reachable.c
@@ -25,6 +25,7 @@ int check_all_reachable(t_game *game)
 {
     char *flood_map;
     int all_reachable;
+    t_map print;
 
     all_reachable = 1;
     flood_map = ft_strdup(game->map->map);
@@ -33,7 +34,10 @@ int check_all_reachable(t_game *game)
         all_reachable = 0;
     if (get_char_count(flood_map, 'E') != 0)
         all_reachable = 0;
-    print_map(flood_map);
+    print.map = flood_map;
+    print.x = game->map->x;
+    print.y = game->map->y;
+    print_map(&print);
     free (flood_map);
     return (all_reachable);
 }
